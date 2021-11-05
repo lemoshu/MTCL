28de1049e55e79145aa96a945d8dd19f313da6c2
lemoshu <315759741@qq.com>
2021-11-06 06:54:48 +0800

    add UDS

--- code/train_MTCL_2D.py
+++ code/train_MTCL_2D.py
@@ -158,34 +158,6 @@ def compute_sdf1_1(img_gt, out_shape):
     return normalized_sdf
 
 
-def compute_sdf(img_gt, out_shape):
-    """
-    compute the signed distance map of binary mask
-    input: segmentation, shape = (batch_size, x, y, z)
-    output: the Signed Distance Map (SDM)
-    sdf(x) = 0; x in segmentation boundary
-             -inf|x-y|; x in segmentation
-             +inf|x-y|; x out of segmentation
-    """
-    img_gt = img_gt.astype(np.uint8)
-
-    gt_sdf = np.zeros(out_shape)
-
-    for b in range(out_shape[0]):
-        for c in range(1, out_shape[1]):
-            posmask = img_gt[b].astype(np.bool)
-            if posmask.any():
-                negmask = ~posmask
-                posdis = distance(posmask)
-                negdis = distance(negmask)
-                boundary = skimage_seg.find_boundaries(posmask, mode='inner').astype(np.uint8)
-                sdf = negdis - posdis
-                sdf[boundary == 1] = 0
-                gt_sdf[b][c] = sdf
-
-    return gt_sdf
-
-
 def boundary_loss(outputs_soft, gt_sdf):
     """
     compute boundary loss for binary segmentation
